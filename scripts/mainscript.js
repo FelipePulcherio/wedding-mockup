8c000d977deb8a6b83816a02af2d119675075c70
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2023-08-28 18:18:20 -0400

    Add condition to run code only in page place.html

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -35,24 +35,25 @@ if (document.URL.includes('index')) {
 }
 
 // Place.html section
+if (document.URL.includes('place')) {
+  function routeButton() {
+    let place_input = document.getElementById('route');
+    console.log(place_input.value);
+    const input_array = place_input.value.split(" ");
+    let new_array = [];
 
-function routeButton() {
-  let place_input = document.getElementById('route');
-  console.log(place_input.value);
-  const input_array = place_input.value.split(" ");
-  let new_array = [];
+    for (let i = 0; i < input_array.length; i++) {
+      new_array.push(input_array[i]);
+      new_array.push("%20");
+    }
+    new_array.pop();
+    new_array = new_array.join("");
 
-  for (let i = 0; i < input_array.length; i++) {
-    new_array.push(input_array[i]);
-    new_array.push("%20");
+    if (place_input.validity.valid && (input_array.length != 0)) {
+      window.open(`https://www.google.com/maps?saddr=${new_array}&daddr=49.21523537499222,-122.90742482946524`);   
+    }
   }
-  new_array.pop();
-  new_array = new_array.join("");
 
-  if (place_input.validity.valid && (input_array.length != 0)) {
-    window.open(`https://www.google.com/maps?saddr=${new_array}&daddr=49.21523537499222,-122.90742482946524`);   
-  }
+  let placeButton = document.getElementById("placeButton");
+  placeButton.addEventListener("click", routeButton);
 }
-
-let placeButton = document.getElementById("placeButton");
-placeButton.addEventListener("click", routeButton);
